932808e937a85d6d1017a45fe40c1fd4657da763
silmaraMoreira <silmaraa.1994@icloud.com>
2024-05-28 08:53:38 -0300

    feat: Domain Validation: NotNull

---
+++ FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.UnitTests/Domain/Validation/DomainValidationTest.cs
@@ -0,0 +1,40 @@
+﻿using Bogus;
+using Xunit;
+using FluentAssertions;
+using FC.Codeflix.Catalog.Domain.Validation;
+using FC.Codeflix.Catalog.Domain.Exceptions;
+
+namespace FC.Codeflix.Catalog.UnitTests.Domain.Validation;
+public class DomainValidationTest
+{
+    private Faker Faker { get; set; } = new Faker();
+
+    // não ser null
+    [Fact(DisplayName = nameof(NotNullOk))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    public void NotNullOk()
+    {
+        var value = Faker.Commerce.ProductName();
+        Action action =
+            () => DomainValidation.NotNull(value, "Value");
+        action.Should().NotThrow();
+    }
+
+    [Fact(DisplayName = nameof(NotNullThowWhenNull))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    public void NotNullThowWhenNull()
+    {
+        string value = null;
+        Action action =
+            () => DomainValidation.NotNull(value, "FieldName");
+        action.Should()
+            .Throw<EntityValidationException>()
+            .WithMessage("FieldName should not be null");
+    }
+
+    //não ser null ou vazio
+    // tamanho minimo
+    //tamanho máximo
+
+
+}
