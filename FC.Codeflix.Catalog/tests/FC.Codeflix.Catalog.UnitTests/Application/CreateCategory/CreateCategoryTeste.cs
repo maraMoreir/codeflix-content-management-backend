b47756353057d4c02695d6f1d970ed1ffca4835f
silmaraMoreira <silmaraa.1994@icloud.com>
2024-05-30 18:03:53 -0300

    feat: starting Use Case CreateCategory Test class

---
+++ FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTeste.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Moq;
+using Xunit;
+using UseCases = FC.Codeflix.Catalog.Application.UseCases.CreateCategory;
+namespace FC.Codeflix.Catalog.UnitTests.Application.CreateCategory;
+
+    [Fact(DisplayName = nameof(CreateCategory))]
+    [Trait("Application", "CreateCategory - Use Cases")]
+    public async void CreateCategory()
+    {
+        var respositoryMock = new Mock<ICategoryRepository>();
+        var unitOfWorkMock = new Mock<IuniOfMock>();
+        var useCase = new UseCases.CreateCategory(
+            respositoryMock.Object,
+            unitOfWorkMock.Object
+        );
+
+        var input = new CreateCategoryInput(
+            "Category Name",
+            "Category Description",
+            true
+            );
+
+        var output = await useCase.Handle(input);
+
+        output.ShouldNotBeNull();
+        output.Name.Should().Be("Category Name");
+        output.Description.Should().Be("Category Description");
+        output.IsActive.Should().Be(true);
+        (output.Id != null && output.Id != Guid.Empty).Should().BeTrue();
+        (output.CreatedAt != null && output.CreatedAt != default(DateTime)).Should().BeTrue();
+}
