f511a147c7192dee896a32db9610c6ff0ed3310a
Silmara Moreira <silmaraa.1994@icloud.com>
2024-07-24 13:44:16 -0300

    feat:  Testing the CreateCategory use case

--- FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTeste.cs
+++ FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTeste.cs
@@ -1,32 +1,50 @@
-﻿using FluentAssertions;
+﻿using FC.Codeflix.Catalog.Domain.Entity;
+using FluentAssertions;
 using Moq;
 using Xunit;
-using UseCases = FC.Codeflix.Catalog.Application.UseCases.CreateCategory;
-namespace FC.Codeflix.Catalog.UnitTests.Application.CreateCategory;
 
-    [Fact(DisplayName = nameof(CreateCategory))]
-    [Trait("Application", "CreateCategory - Use Cases")]
-    public async void CreateCategory()
+namespace FC.Codeflix.Catalog.UnitTests.Application.CreateCategory
+{
+    public class CreateCategoryTest
     {
-        var respositoryMock = new Mock<ICategoryRepository>();
-        var unitOfWorkMock = new Mock<IuniOfMock>();
-        var useCase = new UseCases.CreateCategory(
-            respositoryMock.Object,
-            unitOfWorkMock.Object
-        );
-
-        var input = new CreateCategoryInput(
-            "Category Name",
-            "Category Description",
-            true
+        [Fact(DisplayName = nameof(CreateCategory))]
+        [Trait("Application", "CreateCategory - Use Cases")]
+        public async void CreateCategory()
+        {
+            var repositoryMock = new Mock<ICategoryRepository>();
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var useCase = new UseCases.CreateCategory(
+                repositoryMock.Object,
+                unitOfWorkMock.Object
             );
 
-        var output = await useCase.Handle(input);
+            var input = new CreateCategoryInput(
+                "Category Name",
+                "Category Description",
+                true
+            );
 
-        output.ShouldNotBeNull();
-        output.Name.Should().Be("Category Name");
-        output.Description.Should().Be("Category Description");
-        output.IsActive.Should().Be(true);
-        (output.Id != null && output.Id != Guid.Empty).Should().BeTrue();
-        (output.CreatedAt != null && output.CreatedAt != default(DateTime)).Should().BeTrue();
+            var output = await useCase.Handle(input, CancellationToken.None);
+
+            repositoryMock.Verify(
+                repository => repository.Create(
+                    It.IsAny<Category>(),
+                    It.IsAny<CancellationToken>()
+                ),
+                Times.Once
+            );
+
+            unitOfWorkMock.Verify(
+                uow => uow.Commit(It.IsAny<CancellationToken>()),
+                Times.Once
+            );
+
+            output.ShouldNotBeNull();
+            output.Name.Should().Be("Category Name");
+            output.Description.Should().Be("Category Description");
+            output.IsActive.Should().Be(true);
+            (output.Id != null && output.Id != Guid.Empty).Should().BeTrue();
+            (output.CreatedAt != null && output.CreatedAt != default(DateTime)).Should().BeTrue();
+        }
+    }
 }
