10a051c72ef43b5a7be1fc018fa51e371701165e
Silmara Moreira <silmaraa.1994@icloud.com>
2024-08-08 12:17:51 -0300

    feat: Use Case DeleteCategory Tests

---
+++ FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.UnitTests/Application/DeleteCategory/DeleteCategoryTest.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+using FC.Codeflix.Catalog.Application.UseCases.Category.DeleteCategory;
+using Moq;
+using FluentAssertions;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.DeleteCategory;
+
+[Collection(nameof(DeleteCategoryTestFixture))]
+public class DeleteCategoryTest
+{
+    private readonly DeleteCategoryTestFixture _fixture;
+
+    public DeleteCategoryTest(DeleteCategoryTestFixture fixture)
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(DeleteCategory))]
+    [Trait("Application", "DeleteCategory - UseCases")]
+    public async Task DeleteCategory()
+    {
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var unitOfWorkMock = _fixture.GetUnitOfWorkMock();
+        var exampleGuid = Guid.NewGuid();
+        var categoryExample = _fixture.GetValidCategory();
+        repositoryMock.Setup(x => x.Get(
+            categoryExample.Id,
+        It.IsAny<CancellationToken>())
+        ).ReturnsAsync(categoryExample);
+        var input = new DeleteCategoryInput(categoryExample.Id);
+        var useCase = new DeleteCategory(
+            repositoryMock.Object,
+            unitOfWorkMock.Object
+        );
+
+        await useCase.Handle(input, CancellationToken.None);
+
+        repositoryMock.Verify(x => x.Get(
+            categoryExample.Id,
+            It.IsAny<CancellationToken>()
+        ), Times.Once);
+        repositoryMock.Verify(x => x.Delete(
+           categoryExample.Id,
+           It.IsAny<CancellationToken>()
+       ), Times.Once);
+        unitOfWorkMock.Verify(x => x.Commit(
+            It.IsAny<CancellationToken>()
+        ), Times.Once());
+    }
+}
