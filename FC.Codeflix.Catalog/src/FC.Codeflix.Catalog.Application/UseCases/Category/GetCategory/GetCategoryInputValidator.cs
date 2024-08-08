c7f1dbebe2126acdeae38084d45f05ca6faced5d
Silmara Moreira <silmaraa.1994@icloud.com>
2024-08-08 11:51:20 -0300

    feat:  Finishing GetCategory Validation

--- FC.Codeflix.Catalog/src/FC.Codeflix.Catalog.Application/UseCases/Category/GetCategory/GetCategoryInputValidator.cs
+++ FC.Codeflix.Catalog/src/FC.Codeflix.Catalog.Application/UseCases/Category/GetCategory/GetCategoryInputValidator.cs
@@ -4,5 +4,5 @@ namespace FC.Codeflix.Catalog.Application.UseCases.Category.GetCategory;
 public class GetCategoryInputValidator : AbstractValidator<GetCategoryInput>
 {
     public GetCategoryInputValidator()
-   => RuleFor(x => x.Id).NotEmpty();
+        => RuleFor(x => x.Id).NotEmpty();
 }
