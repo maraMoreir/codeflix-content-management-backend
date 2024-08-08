733c0d93405260921b4fcf083a2a0b8e5f42a928
Silmara Moreira <silmaraa.1994@icloud.com>
2024-08-08 15:26:41 -0300

    feat: UpdateCategory Test

---
+++ FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryTest.cs
@@ -0,0 +1,57 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Category.Common;
+using FluentAssertions;
+using Moq;
+using Xunit;
+using UseCase = FC.Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.UpdateCategory;
+
+[Collection(nameof(UpdateCategoryTestFixture))]
+public class UpdateCategoryTest
+{
+    private readonly UpdateCategoryTestFixture _fixture;
+
+    public UpdateCategoryTest(UpdateCategoryTestFixture fixture)
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(UpdateCategory))]
+    [Trait("Application", "UpdateCategory - Use Cases")]
+    public async Task UpdateCategory()
+    {
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var unitOfWorkMock = _fixture.GetUnitOfWorkMock();
+        var exampleCategory = _fixture.GetExampleCategory();
+        repositoryMock.Setup(x => x.Get(
+            exampleCategory.Id,
+            It.IsAny<CancellationToken>())
+    ).ReturnsAsync(exampleCategory);
+        var input = UseCase.UpdateCategoryInput(
+           exampleCategory.Id,
+           _fixture.GetValidCategoryName(),
+           _fixture.GetValidCategoryDescription(),
+           exampleCategory.IsActive
+        );
+        var useCase = new UseCase.UpdateCategory(
+            repositoryMock.Object,
+            unitOfWorkMock.Object
+        );
+
+        CategoryModelOutut outut = await useCase.Handle(input);
+
+        outut.Should().NotBeNull();
+        outut.Name.Should().Be(input.Name);
+        outut.Description.Should().Be(input.Description);
+        outut.IsActive.Should().Be(input.IsActive);
+        repositoryMock.Verify(x => x.Get(
+            exampleCategory.Id,
+            It.IsAny<CancellationToken>()
+        ), Times.Once);
+        repositoryMock.Verify(x => x.Update(
+            exampleCategory.Id,
+            It.IsAny<CancellationToken>()
+        ), Times.Once);
+        unitOfWorkMock.Verify(x => x.Commit(
+            It.IsAny<CancellationToken>()
+        ), Times.Once);
+    }
+}
---
+++ FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryTestFixture.cs
@@ -0,0 +1,59 @@
+﻿using FC.Codeflix.Catalog.Application.Interfaces;
+using FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+using FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Domain.Repository;
+using FC.Codeflix.Catalog.UnitTests.Common;
+using Moq;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.UpdateCategory;
+
+[CollectionDefinition(nameof(UpdateCategoryTestFixture))]
+public class UpdateCategoryTestFixtureCollection 
+    : ICollectionFixture<UpdateCategoryTestFixture>
+{ }
+public class UpdateCategoryTestFixture 
+    : BaseFixture
+{
+    public Mock<ICategoryRepository> GetRepositoryMock()
+=> new();
+
+    public Mock<IUnitOfWork> GetUnitOfWorkMock()
+        => new();
+
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+        if (categoryDescription.Length > 10_000)
+            categoryDescription = categoryDescription[..10_000];
+        return categoryDescription;
+
+    }
+
+    public bool getRandomBoolean()
+        => (new Random()).NextDouble() < 0.5;
+
+    public CreateCategoryInput GetInput()
+        => new(
+            GetValidCategoryName(),
+            GetValidCategoryDescription(),
+            getRandomBoolean()
+        );
+
+    public Category GetExampleCategory()
+        => new Category(
+            GetValidCategoryName(),
+            GetValidCategoryDescription(),
+            getRandomBoolean()
+        );
+}
