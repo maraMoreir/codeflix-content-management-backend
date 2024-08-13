6778337c46cb39ac057b3128056dcc5a49f16c35
Silmara Moreira <silmaraa.1994@icloud.com>
2024-08-13 19:51:43 -0300

    feat:  UpdateCategoryInput validation

---
+++ FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryInputValidatorTest.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+using FluentAssertions;
+using FC.Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.UpdateCategory;
+
+[Collection(nameof(UpdateCategoryTestFixture))]
+public class UpdateCategoryInputValidatorTest
+{
+    private readonly UpdateCategoryTestFixture _fixture;
+
+    public UpdateCategoryInputValidatorTest(UpdateCategoryTestFixture fixture)
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(DontValidateWhenEmptyGuid))]
+    [Trait("Applicxation", "UpdateCategoryInputValidator - Use Cases")]
+    public void DontValidateWhenEmptyGuid()
+    {
+        var input = _fixture.GetValidInput(Guid.Empty);
+        var validator = new UpdateCategoryInputValidator();
+
+        var validateResult = validator.Validate(input);
+
+        validateResult.Should().NotBeNull();
+        validateResult.IsValid.Should().BeFalse();
+        validateResult.Errors.Should().HaveCount(1);
+        validateResult.Errors[0].ErrorMessage
+            .Should().Be("'Id' deve ser informado.");
+    }
+
+    [Fact(DisplayName = nameof(ValidateWhenValid))]
+    [Trait("Applicxation", "UpdateCategoryInputValidator - Use Cases")]
+    public void ValidateWhenValid()
+    {
+        var input = _fixture.GetValidInput();
+        var validator = new UpdateCategoryInputValidator();
+
+        var validateResult = validator.Validate(input);
+
+        validateResult.Should().NotBeNull();
+        validateResult.IsValid.Should().BeTrue();
+        validateResult.Errors.Should().HaveCount(0);
+    }
+}
