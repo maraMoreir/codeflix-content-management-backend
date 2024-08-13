a303debcd96962ef61a8d6697e7699218997583e
Silmara Moreira <silmaraa.1994@icloud.com>
2024-08-13 20:28:11 -0300

    feat:  Testing the ListCategories use case

---
+++ FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTest.cs
@@ -0,0 +1,50 @@
+﻿using FC.Codeflix.Catalog.Domain.Entity;
+using Moq;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.ListCategories;
+
+[Collection(nameof(ListCategoriesTestFixture))]
+public class ListCategoriesTest
+{
+    private readonly ListCategoriesTestFixture _fixture;
+
+    public ListCategoriesTest(ListCategoriesTestFixture fixture)
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(List))]
+    [Trait("Application", "ListCategories - Use Cases ")]
+    public async Task List()
+    {
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var input = new ListCategoriesInput(
+            page: 2,
+            perPage: 15,
+            search: "search-example",
+            sort: "name",
+            dir: SearchOrder.Asc
+            );
+
+        repositoryMock.Setup(x => x.Search(
+            It.IsAny<SearchInput>(
+                searchInput.Page == input.Page
+                && searchInput.PerPage == input.PerPage
+                && searchInput.Search == input.Search
+                && searchInput.OrderBy == input.Sort
+                && searchInput.Order == input.Dir
+                ),
+            It.IsAny<CancellationToken>()
+            )).ReturnsAsync(new OutpurSearch<Category>(
+                    currentPage: input.Page,
+                    perPage: input.Perpage,
+                    Items: (IReadOnlyList<Category>)_fixture.GetExampleCategoriesList(),
+                    Total: 70
+            ));
+
+        var useCase = new ListCategories(repositoryMock.Object);
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+    }
+}
