9db3263177b288360b50f72a61152a67bc04c60e
Silmara Moreira <silmaraa.1994@icloud.com>
2024-08-23 14:29:35 -0300

    feat: Implementing default values ​​in ListCategoriesInput

--- FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTest.cs
+++ FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTest.cs
@@ -1,4 +1,5 @@
 ﻿using FC.Codeflix.Catalog.Application.UseCases.Category.Common;
+using FC.Codeflix.Catalog.Application.UseCases.Category.ListCategories;
 using FC.Codeflix.Catalog.Domain.Entity;
 using FC.Codeflix.Catalog.Domain.SeedWork.SearchableRepository;
 using FluentAssertions;
@@ -20,19 +21,89 @@ public class ListCategoriesTest
     [Trait("Application", "ListCategories - Use Cases ")]
     public async Task List()
     {
+        var categoriesExampleList = _fixture.GetExampleCategoriesList();
         var repositoryMock = _fixture.GetRepositoryMock();
-        var input = new UseCase.ListCategoriesInput(
-            page: 2,
-            perPage: 15,
-            search: "search-example",
-            sort: "name",
-            dir: SearchOrder.Asc
+        var input = _fixture.GetExampleInput();
+        var outputRepository = new SearchOutput<Category>(
+            currentPage: input.Page,
+            perPage: input.PerPage,
+            items: (IReadOnlyList<Category>)categoriesExampleList,
+            total: 70
             );
         var outputRepositorySearch = new SearchOutput<Category>(
                     currentPage: input.Page,
                     perPage: input.PerPage,
                     items: (IReadOnlyList<Category>)_fixture.GetExampleCategoriesList(),
-                    total: 70
+                    total: (new Random()).Next(50, 200)
+        );
+
+        repositoryMock.Setup(x => x.Search(
+            It.Is<SearchInput>(
+               searchInput => searchInput.Page == input.Page
+                && searchInput.PerPage == input.PerPage
+                && searchInput.Search == input.Search
+                && searchInput.OrderBy == input.Sort
+                && searchInput.Order == input.Dir
+                ),
+            It.IsAny<CancellationToken>()
+            )).ReturnsAsync(outputRepositorySearch);
+
+        var useCase = new UseCase.ListCategories(repositoryMock.Object);
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Page.Should().Be(outputRepositorySearch.CurrentPage);
+        output.PerPage.Should().Be(outputRepositorySearch.PerPage);
+        output.Total.Should().Be(outputRepositorySearch.Total);
+        output.Items.Should().HaveCount(outputRepositorySearch.Items.Count);
+        ((List<CategoryModelOutut>)output.Items).ForEach(outputItem =>
+        {
+            var repositoryCategory = outputRepositorySearch.Items
+                .FirstOrDefault(Xunit => Xunit.Id == outputItem.Id);
+            outputItem.Should().NotBeNull();
+            outputItem.Name.Should().Be(repositoryCategory!.Name);
+            outputItem.Description.Should().Be(repositoryCategory!.Description);
+            outputItem.IsActive.Should().Be(repositoryCategory!.IsActive);
+            outputItem.CreatedAt.Should().Be(repositoryCategory!.CreatedAt);
+        });
+
+        repositoryMock.Verify(x => x.Search(
+            It.Is<SearchInput>(
+                searchInput => searchInput.Page == input.Page
+                && searchInput.PerPage == input.PerPage
+                && searchInput.Search == input.Search
+                && searchInput.OrderBy == input.Sort
+                && searchInput.Order == input.Dir
+                ),
+            It.IsAny<CancellationToken>()
+            ), Times.Once);
+    }
+
+    [Theory(DisplayName = nameof(ListInputWithotAllParameters))]
+    [Trait("Application", "ListCategories - Use Cases")]
+    [MemberData(
+        nameof(ListCategoriesTestDataGenerator.GetInputsWithoutAllParameters),
+        parameters:14,
+        MemberType = typeof(ListCategoriesTestDataGenerator)
+    )]
+    public async Task ListInputWithotAllParameters(
+        UseCase.ListCategoriesInput input
+    )
+    {
+        var categoriesExampleList = _fixture.GetExampleCategoriesList();
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var outputRepository = new SearchOutput<Category>(
+            currentPage: input.Page,
+            perPage: input.PerPage,
+            items: (IReadOnlyList<Category>)categoriesExampleList,
+            total: 70
+            );
+        var outputRepositorySearch = new SearchOutput<Category>(
+                    currentPage: input.Page,
+                    perPage: input.PerPage,
+                    items: (IReadOnlyList<Category>)_fixture.GetExampleCategoriesList(),
+                    total: (new Random()).Next(50, 200)
         );
 
         repositoryMock.Setup(x => x.Search(
---
+++ FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTestDataGenerator.cs
@@ -0,0 +1,65 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Category.ListCategories;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.ListCategories;
+public class ListCategoriesTestDataGenerator
+{
+    public static IEnumerable<object[]> GetInputsWithoutAllParameters(int times = 14)
+    {
+        var fixture = new ListCategoriesTestFixture();
+        var inputExample = fixture.GetExampleInput();
+        for ( int i = 0; i < times; i ++)
+        {
+            switch ( i % 7)
+            {
+                case 0:
+                    yield return new object[] {
+                        new ListCategoriesInput()
+                    };
+                    break;
+                case 1:
+                    yield return new object[] { 
+                        new ListCategoriesInput(inputExample.Page)
+                    };
+                    break;
+                case 2:
+                    yield return new object[] { 
+                        new ListCategoriesInput(
+                            inputExample.Page,
+                            inputExample.PerPage
+                       )
+                    };
+                    break;
+                case 3:
+                    yield return new object[] {
+                        new ListCategoriesInput(
+                            inputExample.Page,
+                            inputExample.PerPage,
+                            inputExample.Search
+                            ) 
+                    };
+                    break;
+                case 4:
+                    yield return new object[] {
+                        new ListCategoriesInput(
+                            inputExample.Page,
+                            inputExample.PerPage,
+                            inputExample.Search,
+                            inputExample.Sort
+                            )
+                    };
+                    break;
+                case 5:
+                    yield return new object[] { inputExample };
+                    break;
+                default:
+                    yield return new object[]
+                    {
+                        new ListCategoriesInput()
+                    };
+                    break;
+            }
+        }
+
+    }
+}
