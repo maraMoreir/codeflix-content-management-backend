b677c789d3411c80c06a58aa04b3f00dbbb1b091
Silmara Moreira <silmaraa.1994@icloud.com>
2024-08-24 22:27:07 -0300

    feat: Infra - Creating the basis for CategoryRepository testing

---
+++ FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTest.cs
@@ -0,0 +1,8 @@
+﻿using Xunit;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Infra.Data.EF.Repositories.CategoryRepository;
+
+[Collection(nameof(CategoryRepositoryTestFixture))]
+public class CategoryRepositoryTest 
+{
+}
---
+++ FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTestFixture.cs
@@ -0,0 +1,13 @@
+﻿using FC.Codeflix.Catalog.IntegrationTests.Base;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Infra.Data.EF.Repositories.CategoryRepository;
+
+[CollectionDefinition(nameof(CategoryRepositoryTestFixture))]
+public class CategoryRepositoryTestFixureCollection
+    : ICollectionFixture<CategoryRepositoryTestFixture>
+{ }
+
+public class CategoryRepositoryTestFixture 
+    : BaseFixture
+{ }
