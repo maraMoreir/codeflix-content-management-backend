36cf8dc8efba47aa498fd954923269104790ad36
Silmara Moreira <silmaraa.1994@icloud.com>
2024-11-12 16:46:35 -0300

    test(integration): Integrated testing of the UpdateCategory use case

---
+++ FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Category/UpdateCategory/UpdateCategoryTestDataGenerator.cs
@@ -0,0 +1,51 @@
+﻿namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.UpdateCategory;
+public class UpdateCategoryTestDataGenerator
+{
+    public static IEnumerable<object[]> GetCategoriesToUpdate(int times = 10)
+    {
+        var fixture = new UpdateCategoryTestFixture();
+        for (int indice = 0; indice < times; indice++)
+        {
+            var exampleCategory = fixture.GetExampleCategory();
+            var exampleInput = fixture.GetValidInput(exampleCategory.Id);
+            yield return new object[] {
+                exampleCategory, exampleInput
+            };
+        }
+    }
+
+    public static IEnumerable<object[]> GetInvalidInputs(int times = 12)
+    {
+        var fixture = new UpdateCategoryTestFixture();
+        var invalidInputsList = new List<object[]>();
+        var totalInvalidCases = 3;
+
+        for (int index = 0; index < times; index++)
+        {
+            switch (index % totalInvalidCases)
+            {
+                case 0:
+                    invalidInputsList.Add(new object[] {
+                        fixture.GetInvalidInputShortName(),
+                        "Name should be at least 3 characters long"
+                    });
+                    break;
+                case 1:
+                    invalidInputsList.Add(new object[] {
+                        fixture.GetInvalidInputTooLongName(),
+                        "Name should be or less or equal 255 characters long"
+                    });
+                    break;
+                case 2:
+                    invalidInputsList.Add(new object[] {
+                        fixture.GetInvalidInputTooLongDescription(),
+                        "Description should be less or equal 10_000 characters long"
+                    });
+                    break;
+                default:
+                    break;
+            }
+        }
+        return invalidInputsList;
+    }
+}
