81312be2589db92fe4998d8ea3ab605d38094927
Silmara Moreira <silmaraa.1994@icloud.com>
2024-11-13 15:32:57 -0300

    test(integration): Integrated test of the ListCategories use case

---
+++ FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Category/ListCategories/ListCategoriesTestFixture.cs
@@ -0,0 +1,43 @@
+﻿using FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.Common;
+using FC.Codeflix.Catalog.Domain.SeedWork.SearchableRepository;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.ListCategories;
+
+[CollectionDefinition(nameof(ListCategoriesTestFixture))]
+public class ListCategoriesTestFixtureCollection
+    : ICollectionFixture<ListCategoriesTestFixture>
+{ }
+public class ListCategoriesTestFixture
+    : CategoryUseCasesBaseFixture
+{
+    public List<DomainEntity.Category> GetExampleCategoriesListWithNames(
+        List<string> names
+    ) => names.Select(name =>
+    {
+        var category = GetExampleCategory();
+        category.Update(name);
+        return category;
+    }).ToList();
+
+    public List<DomainEntity.Category> CloneCategoriesListOrderd(
+        List<DomainEntity.Category> categoriesList,
+        string orderBy,
+        SearchOrder order
+    )
+    {
+        var listClone = new List<DomainEntity.Category>(categoriesList);
+        var orderdEnumerable = (orderBy.ToLower(), order) switch
+        {
+            ("name", SearchOrder.Asc) => listClone.OrderBy(x => x.Name),
+            ("name", SearchOrder.Desc) => listClone.OrderByDescending(x => x.Name),
+            ("id", SearchOrder.Asc) => listClone.OrderBy(x => x.Id),
+            ("id", SearchOrder.Desc) => listClone.OrderByDescending(x => x.Id),
+            ("createdat", SearchOrder.Asc) => listClone.OrderBy(x => x.CreatedAt),
+            ("createdat", SearchOrder.Desc) => listClone.OrderByDescending(x => x.CreatedAt),
+            _ => listClone.OrderBy(x => x.Name),
+        };
+        return orderdEnumerable.ToList();
+    }
+}
