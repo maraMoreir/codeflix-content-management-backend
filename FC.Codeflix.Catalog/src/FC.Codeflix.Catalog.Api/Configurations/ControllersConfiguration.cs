bae5f2e1e668520a59e720a61aa3d3ac0a1a2ab8
silmaraa.1994@icloud.com <silmaraa.1994@icloud.com>
2025-01-08 15:17:03 -0300

    feat: api setup

---
+++ FC.Codeflix.Catalog/src/FC.Codeflix.Catalog.Api/Configurations/ControllersConfiguration.cs
@@ -0,0 +1,32 @@
+﻿namespace FC.Codeflix.Catalog.Api.Configurations;
+
+public static class ControllersConfiguration
+{
+    public static IServiceCollection AddAndConfigureControllers(
+        this IServiceCollection services
+    ) {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        return services;
+    }
+
+    private static IServiceCollection AddDocumentation(
+        this IServiceCollection services
+    ) {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        return services;
+    }
+
+    public static WebApplication UseDocumentation(
+        this WebApplication app 
+    ) {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        return app;
+    }
+}
---
+++ FC.Codeflix.Catalog/src/FC.Codeflix.Catalog.Api/Configurations/UseCasesConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using FC.Codeflix.Catalog.Application.Interfaces;
+using FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+using FC.Codeflix.Catalog.Domain.Repository;
+using FC.Codeflix.Catalog.Infra.Data.EF;
+using FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
+using MediatR;
+
+namespace FC.Codeflix.Catalog.Api.Configurations;
+
+public static class UseCasesConfiguration
+{
+    public static IServiceCollection AddUseCases(
+        this IServiceCollection services
+    ) {
+        services.AddMediatR(typeof(CreateCategory));
+        services.AddRepositories();
+        return services;
+    }
+
+    public static IServiceCollection AddRepositories(
+        this IServiceCollection services
+    ) {
+        services.AddTransient<
+            ICategoryRepository, CategoryRepository>();
+        services.AddTransient<IUnitOfWork, UnitOfWork>();
+        return services;
+    }
+}
