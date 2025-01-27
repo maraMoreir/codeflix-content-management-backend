e47da890e35f83524efdaa1b39213d97b2683383
silmaraa.1994@icloud.com <silmaraa.1994@icloud.com>
2025-01-27 11:40:19 -0300

    test(end-2-end): endpoint test update category

---
+++ FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.EndToEnd.Tests/Api/Category/UpdateCategory/UpdateCategoryApiTest.cs
@@ -0,0 +1,46 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Category.Common;
+using Microsoft.AspNetCore.Http;
+using FluentAssertions;
+using System.Net;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.EndToEnd.Tests.Api.Category.UpdateCategory;
+
+[Collection(nameof(UpdateCategoryApiTestFixture))]
+public class UpdateCategoryApiTest
+{
+    private readonly UpdateCategoryApiTestFixture _fixture;
+
+    public UpdateCategoryApiTest(UpdateCategoryApiTestFixture fixture)
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(UpdateCategory))]
+    [Trait("EndToEnd/API", "Category/Update - Endpoints")]
+    public async void UpdateCategory()
+    {
+        var exampleCategoriesList = _fixture.GetExampleCategoryList(20);
+        await _fixture.Persistence.InsertList(exampleCategoriesList);
+        var exampleCategory = exampleCategoriesList[10];
+        var input = _fixture.GetExampleInput(exampleCategory.Id);
+
+        var (response, output) = await _fixture.ApiClient.Put<CategoryModelOutut>(
+            $"/categories/{exampleCategory.Id}",
+            input
+        );
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status200OK);
+        output.Should().NotBeNull();
+        output!.Id.Should().Be(exampleCategory.Id);
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().Be((bool)input.IsActive!);
+        output.Id.Should().NotBeEmpty();
+        var dbCategory = await _fixture
+            .Persistence.GetById(exampleCategory.Id);
+        dbCategory.Should().NotBeNull();
+        dbCategory!.Name.Should().Be(input.Name);
+        dbCategory.Description.Should().Be(input.Description);
+        dbCategory.IsActive.Should().Be((bool)input.IsActive!);
+    }
+}
---
+++ FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.EndToEnd.Tests/Api/Category/UpdateCategory/UpdateCategoryApiTestFixture.cs
@@ -0,0 +1,22 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
+using FC.Codeflix.Catalog.EndToEnd.Tests.Api.Category.Common;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.EndToEnd.Tests.Api.Category.UpdateCategory;
+
+[CollectionDefinition(nameof(UpdateCategoryApiTestFixture))]
+public class UpdateCategoryApiTestCollection
+    : ICollectionFixture<UpdateCategoryApiTestFixture>
+{ }
+
+public class UpdateCategoryApiTestFixture
+    : CategoryBaseFixture
+{
+    public UpdateCategoryInput GetExampleInput(Guid? id = null)
+        => new(
+            id ?? Guid.NewGuid(),
+            GetValidCategoryName(),
+            GetValidCategoryDescription(),
+            getRandomBoolean()
+        );
+}
