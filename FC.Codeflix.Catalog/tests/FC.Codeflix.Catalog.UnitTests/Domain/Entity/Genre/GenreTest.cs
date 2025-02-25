d76f52d118224f90c3790800ca31563e395ae141
silmaraa.1994@icloud.com <silmaraa.1994@icloud.com>
2025-02-25 15:10:17 -0300

    test(unit): start the genre aggregate

---
+++ FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Genre/GenreTest.cs
@@ -0,0 +1,47 @@
+﻿using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+using FluentAssertions;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Domain.Entity.Genre;
+
+[Collection(nameof(GenreTestFixture))]
+public class GenreTest
+{
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "Genre - Aggregates")]
+    public void Instantiate()
+    {
+        var genreName = "Horror";
+
+        var datetimeBefore = DateTime.Now;
+        var genre = new DomainEntity.Genre(genreName);
+        var datetimeAfter = DateTime.Now.AddSeconds(1);
+
+        genre.Should().NotBeNull();
+        genre.Name.Should().Be(genreName);
+        genre.IsActive.Should().BeTrue();
+        genre.CreatedAt.Should().NotBeSameDateAs(default);
+        (genre.CreatedAt > datetimeBefore).Should().BeTrue();
+        (genre.CreatedAt < datetimeAfter).Should().BeTrue();
+    }
+
+    [Theory(DisplayName = nameof(InstantiateWithIsActive))]
+    [Trait("Domain", "Genre - Aggregates")]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void InstantiateWithIsActive(bool isActive)
+    {
+        var genreName = "Horror";
+
+        var datetimeBefore = DateTime.Now;
+        var genre = new DomainEntity.Genre(genreName, isActive);
+        var datetimeAfter = DateTime.Now.AddSeconds(1);
+
+        genre.Should().NotBeNull();
+        genre.Name.Should().Be(genreName);
+        genre.IsActive.Should().Be(isActive);
+        genre.CreatedAt.Should().NotBeSameDateAs(default);
+        (genre.CreatedAt > datetimeBefore).Should().BeTrue();
+        (genre.CreatedAt < datetimeAfter).Should().BeTrue();
+    }
+}
---
+++ FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Genre/GenreTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using FC.Codeflix.Catalog.UnitTests.Common;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Domain.Entity.Genre;
+public class GenreTestFixtureCollection
+    : ICollectionFixture<GenreTestFixture>
+{ }
+
+public class GenreTestFixture
+    : BaseFixture
+{ }
